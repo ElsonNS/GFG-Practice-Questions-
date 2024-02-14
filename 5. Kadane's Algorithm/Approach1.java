682fab0e8c61acd0c9c4a444e162857ad37a2e6b
ElsonNS <nselson26@gmail.com>
2024-02-14 22:39:25 +0530

    git commit -O(n) "Kadane's Algorithm"

---
+++ 5. Kadane's Algorithm/Approach1.java
@@ -0,0 +1,22 @@
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        {      
+        long res =arr[0], max =res;
+
+
+        for(int i=1;i<n;i++)
+            {
+                max = Math.max(max+arr[i],arr[i]);
+                res = Math.max(max,res);
+            }
+            
+            return res;
+  }
+}
+}
